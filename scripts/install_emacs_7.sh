06a4faebcf2b9c5a188a1adb15cee089e10f0ae1
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2022-04-25 17:25:33 +0200

    Update install_emacs_7.sh

--- scripts/install_emacs_7.sh
+++ scripts/install_emacs_7.sh
@@ -2,12 +2,30 @@
 
 non_interactive="DEBIAN_FRONTEND=noninteractive"
 
-# Install elpy requirements
-sudo $non_interactive apt-get install virtualenv black flake8 pylint mypy -y
+if ! [ -x "$(command -v emacs)" ]; then
 
-# Install emacs
-sudo $non_interactive apt-get install emacs -y
+  # Install elpy requirements
+  sudo $non_interactive apt-get install virtualenv black flake8 pylint mypy -y
+  python3 -m pip install jedi 
 
-sudo pip install jedi 
+  # Install emacs
+  sudo $non_interactive apt-get build-dep emacs -y
+  
+  
+  mkdir /tmp/install_emacs
 
-cp ./emacs/.emacs ~/
+  git clone https://github.com/emacs-mirror/emacs.git /tmp/install_emacs
+
+  /tmp/install_emacs/autogen.sh CFLAGS="-ggdb3 -O0" CXXFLAGS="-ggdb3 -O0" LDFLAGS="-ggdb3" 
+  /tmp/install_emacs/configure --with-modules --with-json
+
+  cd /tmp/install_emacs/
+
+  bear make -j $(nproc)
+
+  sudo make install
+
+  sudo rm -rf /tmp/install_emacs
+
+
+fi
