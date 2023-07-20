ac208f567c66f5ea04e4b26793858052bc63e353
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2023-07-20 21:29:53 +0200

    Update install_iptables_1.sh

--- scripts/install_iptables_1.sh
+++ scripts/install_iptables_1.sh
@@ -10,22 +10,21 @@ echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo deb
 
 sudo apt-get install iptables-persistent -y
 
-# Allow UDP 
-sudo iptables -I INPUT -p udp --sport 53 -j ACCEPT
-sudo iptables -I INPUT -p tcp --sport 53 -j ACCEPT
+# Allow DNS
+sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+sudo iptables -A INPUT -p udp --sport 53 -j ACCEPT
 # Allow HTTP
-## For all output packages
-sudo iptables -A OUTPUT -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-## For input packages which are going back
-sudo iptables -A INPUT -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
 # Allow HTTPS
-## For all output packages
-sudo iptables -A OUTPUT -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-## For input packages which are going back
-sudo iptables -A INPUT -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
 # Allow NTP
 sudo iptables -A INPUT -p udp -m udp --sport 123 -j ACCEPT
 sudo iptables -A OUTPUT -p udp -m udp --dport 123 -j ACCEPT
+# Allow localhost
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A OUTPUT -o lo -j ACCEPT
 
 # Save iptables
 sudo iptables-save | sudo tee /etc/iptables/rules.v4 > /dev/null
