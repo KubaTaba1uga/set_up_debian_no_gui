a29bfa6117597fb8f3b38e3a51a4ab51d5729a2c
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2023-07-20 21:26:55 +0200

    Update install_ssh_2.sh

--- scripts/install_ssh_2.sh
+++ scripts/install_ssh_2.sh
@@ -11,8 +11,8 @@ sudo echo "PermitRootLogin no" | sudo tee -a /etc/ssh/sshd_config > /dev/null
 sudo echo "PubkeyAuthentication yes" | sudo tee -a /etc/ssh/sshd_config > /dev/null
 
 # Allow port 22
-sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-sudo iptables -A INPUT -p tcp --sport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
 # Drop incoming packages
 sudo iptables -P INPUT DROP
